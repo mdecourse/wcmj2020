bbfe706e2da4baa18c42543152f6bb29d8d3a5d8
mdecourse <mdecourse@gmail.com>
2020-01-16 16:17:55 +0800

    add semester calendar

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "CMSiMDE"
+    site_title = "WCMj2020 網際內容管理"
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
